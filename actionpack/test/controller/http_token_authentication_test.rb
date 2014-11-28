sivagollapalli/rails

commit d67b289796bd121bfe8d2b5d20d157a011116dfe

update ActiveModel::Serializers documentation [ci skip]

activemodel/lib/active_model/serializers/json.rb
@@ -119,20 +119,22 @@ module ActiveModel
       #     end
       #   end
       #
+      #   json = { name: 'bob', age: 22, awesome:true }.to_json
       #   person = Person.new
-      #   person.from_json("{\"name\":\"bob\",\"age\":22,\"awesome\":true}")
-      #   person.name    # => "bob"
-      #   person.age     # => 22
-      #   person.awesome # => true
+      #   person.from_json(json) # => #<Person:0x007fec5e7a0088 @age=22, @awesome=true, @name="bob">
+      #   person.name            # => "bob"
+      #   person.age             # => 22
+      #   person.awesome         # => true
       #
       # The default value for +include_root+ is +false+. You can change it to
       # +true+ if the given JSON string includes a single root node.
       #
+      #   json = { person: { name: 'bob', age: 22, awesome:true } }.to_json
       #   person = Person.new
-      #   person.from_json("{\"person\":{\"name\":\"bob\",\"age\":22,\"awesome\":true}}", true)
-      #   person.name    # => "bob"
-      #   person.age     # => 22
-      #   person.awesome # => true
+      #   person.from_json(json) # => #<Person:0x007fec5e7a0088 @age=22, @awesome=true, @name="bob">
+      #   person.name            # => "bob"
+      #   person.age             # => 22
+      #   person.awesome         # => true
       def from_json(json, include_root=include_root_in_json)
         hash = ActiveSupport::JSON.decode(json)
         hash = hash.values.first if include_root

activemodel/lib/active_model/serializers/xml.rb
@@ -110,7 +110,7 @@ module ActiveModel
           end
         end
 
-        # TODO This can likely be cleaned up to simple use ActiveSupport::XmlMini.to_tag as well.
+        # TODO: This can likely be cleaned up to simple use ActiveSupport::XmlMini.to_tag as well.
         def add_associations(association, records, opts)
           merged_options = opts.merge(options.slice(:builder, :indent))
           merged_options[:skip_instruct] = true
@@ -161,8 +161,8 @@ module ActiveModel
       # Returns XML representing the model. Configuration can be
       # passed through +options+.
       #
-      # Without any +options+, the returned XML string will include all the model's
-      # attributes. For example:
+      # Without any +options+, the returned XML string will include all the
+      # model's attributes.
       #
       #   user = User.find(1)
       #   user.to_xml
@@ -175,18 +175,42 @@ module ActiveModel
       #     <created-at type="dateTime">2011-01-30T22:29:23Z</created-at>
       #   </user>
       #
-      # The <tt>:only</tt> and <tt>:except</tt> options can be used to limit the attributes
-      # included, and work similar to the +attributes+ method.
+      # The <tt>:only</tt> and <tt>:except</tt> options can be used to limit the
+      # attributes included, and work similar to the +attributes+ method.
       #
       # To include the result of some method calls on the model use <tt>:methods</tt>.
       #
       # To include associations use <tt>:include</tt>.
       #
-      # For further documentation see activerecord/lib/active_record/serializers/xml_serializer.xml.
+      # For further documentation, see activerecord/lib/active_record/serializers/xml_serializer.xml.
       def to_xml(options = {}, &block)
         Serializer.new(self, options).serialize(&block)
       end
 
+      # Sets the model +attributes+ from a JSON string. Returns +self+.
+      #
+      #   class Person
+      #     include ActiveModel::Serializers::Xml
+      #
+      #     attr_accessor :name, :age, :awesome
+      #
+      #     def attributes=(hash)
+      #       hash.each do |key, value|
+      #         instance_variable_set("@#{key}", value)
+      #       end
+      #     end
+      #
+      #     def attributes
+      #       instance_values
+      #     end
+      #   end
+      #
+      #   xml = { name: 'bob', age: 22, awesome:true }.to_xml
+      #   person = Person.new
+      #   person.from_xml(xml) # => #<Person:0x007fec5e3b3c40 @age=22, @awesome=true, @name="bob">
+      #   person.name          # => "bob"
+      #   person.age           # => 22
+      #   person.awesome       # => true
       def from_xml(xml)
         self.attributes = Hash.from_xml(xml).values.first
         self

commit a3fdc41c2158bdd58e087a1c00609fa779af9349

Changed to assert from assert_equal
+ Added msg to assertion

Refactor nested if

Provide support for SQL Server connections with dbconsole using sqsh

Active Record change log pass [skip ci]

Use absolute_path of caller_locations to infer engine root

According to documentation `path` only returns file names. On MRI it's
not the case but it's likely a bug in MRI.

Add #record attribute to RecordNotFound and RecordDestroyed exceptions.

This allows these exceptions to be handled generically in conjunction with RecordInvalid.

be more general with adapter name

various error classes: added newlines & removed :nodoc: flag from public attribute.

Allow authentication header to not have to specify 'token=' key.

Fixes: https://github.com/rails/rails/issues/17108.

Fixing documentation in JSON#from_json

Commit d67b289 introduced a tiny regression in the docs for #from_json,
true needs to be included when the root node is present.

Test against rack master

pass over the release notes

Fix typo in Rails 4.2 release notes

Fix format "Form Helpers" guide

The `"_tag"` was interpreted as the start of a italics.

edits in the release notes for 4.2

removes double mention of deliver_now

rails -> bin/rails in index.html

updates some pointers in the guides welcome section [ci skip]

Modified failure msg

Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gemspec
 
+gem 'rack', github: 'rack/rack'
+
 # We need a newish Rake since Active Job sets its test tasks' descriptions.
 gem 'rake', '>= 10.3'
 

actionpack/lib/action_controller/metal/http_authentication.rb
@@ -397,6 +397,7 @@ module ActionController
     #
     #   RewriteRule ^(.*)$ dispatch.fcgi [E=X-HTTP_AUTHORIZATION:%{HTTP:Authorization},QSA,L]
     module Token
+      TOKEN_KEY = 'token='
       TOKEN_REGEX = /^Token /
       AUTHN_PAIR_DELIMITERS = /(?:,|;|\t+)/
       extend self
@@ -471,7 +472,13 @@ module ActionController
       # pairs by the standardized <tt>:</tt>, <tt>;</tt>, or <tt>\t</tt>
       # delimiters defined in +AUTHN_PAIR_DELIMITERS+.
       def raw_params(auth)
-        auth.sub(TOKEN_REGEX, '').split(/\s*#{AUTHN_PAIR_DELIMITERS}\s*/)
+        _raw_params = auth.sub(TOKEN_REGEX, '').split(/\s*#{AUTHN_PAIR_DELIMITERS}\s*/)
+
+        if !(_raw_params.first =~ %r{\A#{TOKEN_KEY}})
+          _raw_params[0] = "#{TOKEN_KEY}#{_raw_params.first}"
+        end
+
+        _raw_params
       end
 
       # Encodes the given token and options into an Authorization header value.
@@ -481,7 +488,7 @@ module ActionController
       #
       # Returns String.
       def encode_credentials(token, options = {})
-        values = ["token=#{token.to_s.inspect}"] + options.map do |key, value|
+        values = ["#{TOKEN_KEY}#{token.to_s.inspect}"] + options.map do |key, value|
           "#{key}=#{value.to_s.inspect}"
         end
         "Token #{values * ", "}"

actionpack/lib/action_dispatch/routing/mapper.rb
@@ -244,12 +244,10 @@ module ActionDispatch
           def app(blocks)
             if to.respond_to?(:call)
               Constraints.new(to, blocks, false)
+            elsif blocks.any?
+              Constraints.new(dispatcher(defaults), blocks, true)
             else
-              if blocks.any?
-                Constraints.new(dispatcher(defaults), blocks, true)
-              else
-                dispatcher(defaults)
-              end
+              dispatcher(defaults)
             end
           end
 

activemodel/lib/active_model/serializers/json.rb
@@ -130,10 +130,10 @@ module ActiveModel
       #
       #   json = { person: { name: 'bob', age: 22, awesome:true } }.to_json
       #   person = Person.new
-      #   person.from_json(json) # => #<Person:0x007fec5e7a0088 @age=22, @awesome=true, @name="bob">
-      #   person.name            # => "bob"
-      #   person.age             # => 22
-      #   person.awesome         # => true
+      #   person.from_json(json, true) # => #<Person:0x007fec5e7a0088 @age=22, @awesome=true, @name="bob">
+      #   person.name                  # => "bob"
+      #   person.age                   # => 22
+      #   person.awesome               # => true
       def from_json(json, include_root=include_root_in_json)
         hash = ActiveSupport::JSON.decode(json)
         hash = hash.values.first if include_root

activerecord/lib/active_record/errors.rb
@@ -52,10 +52,29 @@ module ActiveRecord
   # Raised by ActiveRecord::Base.save! and ActiveRecord::Base.create! methods when record cannot be
   # saved because record is invalid.
   class RecordNotSaved < ActiveRecordError
+    attr_reader :record
+
+    def initialize(record)
+      @record = record
+      super()
+    end
   end
 
   # Raised by ActiveRecord::Base.destroy! when a call to destroy would return false.
+  #
+  #   begin
+  #     complex_operation_that_internally_calls_destroy!
+  #   rescue ActiveRecord::RecordNotDestroyed => invalid
+  #     puts invalid.record.errors
+  #   end
+  #
   class RecordNotDestroyed < ActiveRecordError
+    attr_reader :record
+
+    def initialize(record)
+      @record = record
+      super()
+    end
   end
 
   # Superclass for all database execution errors.
@@ -177,6 +196,7 @@ module ActiveRecord
   # offending attribute.
   class AttributeAssignmentError < ActiveRecordError
     attr_reader :exception, :attribute
+
     def initialize(message, exception, attribute)
       super(message)
       @exception = exception
@@ -189,6 +209,7 @@ module ActiveRecord
   # objects, each corresponding to the error while assigning to an attribute.
   class MultiparameterAssignmentErrors < ActiveRecordError
     attr_reader :errors
+
     def initialize(errors)
       @errors = errors
     end

activerecord/lib/active_record/persistence.rb
@@ -139,7 +139,7 @@ module ActiveRecord
     # Attributes marked as readonly are silently ignored if the record is
     # being updated.
     def save!(*)
-      create_or_update || raise(RecordNotSaved)
+      create_or_update || raise(RecordNotSaved, self)
     end
 
     # Deletes the record in the database and freezes this instance to
@@ -181,7 +181,7 @@ module ActiveRecord
     # and <tt>destroy!</tt> raises ActiveRecord::RecordNotDestroyed. See
     # ActiveRecord::Callbacks for further details.
     def destroy!
-      destroy || raise(ActiveRecord::RecordNotDestroyed)
+      destroy || raise(ActiveRecord::RecordNotDestroyed, self)
     end
 
     # Returns an instance of the specified +klass+ with the attributes of the

activerecord/lib/active_record/validations.rb
@@ -5,13 +5,14 @@ module ActiveRecord
   # +record+ method to retrieve the record which did not validate.
   #
   #   begin
-  #     complex_operation_that_calls_save!_internally
+  #     complex_operation_that_internally_calls_save!
   #   rescue ActiveRecord::RecordInvalid => invalid
   #     puts invalid.record.errors
   #   end
   class RecordInvalid < ActiveRecordError
-    attr_reader :record # :nodoc:
-    def initialize(record) # :nodoc:
+    attr_reader :record
+
+    def initialize(record)
       @record = record
       errors = @record.errors.full_messages.join(", ")
       super(I18n.t(:"#{@record.class.i18n_scope}.errors.messages.record_invalid", :errors => errors, :default => :"errors.messages.record_invalid"))

guides/source/_welcome.html.erb
@@ -15,5 +15,5 @@
 </p>
 <% end %>
 <p>
-  The guides for earlier releases: <a href="http://guides.rubyonrails.org/v4.1.6/">Rails 4.1.6</a>, <a href="http://guides.rubyonrails.org/v4.0.10/">Rails 4.0.10</a>, <a href="http://guides.rubyonrails.org/v3.2.19/">Rails 3.2.19</a> and <a href="http://guides.rubyonrails.org/v2.3.11/">Rails 2.3.11</a>.
+  The guides for earlier releases: <a href="http://guides.rubyonrails.org/v4.1.8/">Rails 4.1.8</a>, <a href="http://guides.rubyonrails.org/v4.0.12/">Rails 4.0.12</a>, <a href="http://guides.rubyonrails.org/v3.2.21/">Rails 3.2.21</a> and <a href="http://guides.rubyonrails.org/v2.3.11/">Rails 2.3.11</a>.
 </p>

railties/lib/rails/commands/dbconsole.rb
@@ -44,7 +44,7 @@ module Rails
 
         find_cmd_and_exec(['mysql', 'mysql5'], *args)
 
-      when "postgresql", "postgres", "postgis"
+      when /postgres|postgis/
         ENV['PGUSER']     = config["username"] if config["username"]
         ENV['PGHOST']     = config["host"] if config["host"]
         ENV['PGPORT']     = config["port"].to_s if config["port"]
@@ -73,6 +73,21 @@ module Rails
         end
 
         find_cmd_and_exec('sqlplus', logon)
+
+      when "sqlserver"
+        args = []
+
+        args += ["-D", "#{config['database']}"] if config['database']
+        args += ["-U", "#{config['username']}"] if config['username']
+        args += ["-P", "#{config['password']}"] if config['password']
+
+        if config['host']
+          host_arg = "#{config['host']}"
+          host_arg << ":#{config['port']}" if config['port']
+          args += ["-S", host_arg]
+        end
+
+        find_cmd_and_exec("sqsh", *args)
 
       else
         abort "Unknown command-line client for #{config['database']}. Submit a Rails patch to add support!"

railties/lib/rails/engine.rb
@@ -351,7 +351,7 @@ module Rails
 
           base.called_from = begin
             call_stack = if Kernel.respond_to?(:caller_locations)
-              caller_locations.map(&:path)
+              caller_locations.map(&:absolute_path)
             else
               # Remove the line number from backtraces making sure we don't leave anything behind
               caller.map { |p| p.sub(/:\d+.*/, '') }

railties/lib/rails/generators/app_base.rb
@@ -195,9 +195,9 @@ module Rails
 
       def rails_gemfile_entry
         if options.dev?
-          [GemfileEntry.path('rails', Rails::Generators::RAILS_DEV_PATH)]
+          [GemfileEntry.path('rails', Rails::Generators::RAILS_DEV_PATH), GemfileEntry.github('rack', 'rack/rack')]
         elsif options.edge?
-          [GemfileEntry.github('rails', 'rails/rails')]
+          [GemfileEntry.github('rails', 'rails/rails'), GemfileEntry.github('rack', 'rack/rack')]
         else
           [GemfileEntry.version('rails',
                             Rails::VERSION::STRING,

railties/lib/rails/templates/rails/welcome/index.html.erb
@@ -237,7 +237,7 @@
 
           <ol>
             <li>
-              <h2>Use <code>rails generate</code> to create your models and controllers</h2>
+              <h2>Use <code>bin/rails generate</code> to create your models and controllers</h2>
               <p>To see all available options, run it without parameters.</p>
             </li>
 

actionpack/test/controller/http_token_authentication_test.rb
@@ -162,17 +162,36 @@ class HttpTokenAuthenticationTest < ActionController::TestCase
     assert_equal(expected, actual)
   end
 
+  test "token_and_options returns right token when token key is not specified in header" do
+    token = "rcHu+HzSFw89Ypyhn/896A="
+
+    actual = ActionController::HttpAuthentication::Token.token_and_options(
+      sample_request_without_token_key(token)
+    ).first
+
+    expected = token
+    assert_equal(expected, actual)
+  end
+
   private
 
     def sample_request(token, options = {nonce: "def"})
       authorization = options.inject([%{Token token="#{token}"}]) do |arr, (k, v)|
         arr << "#{k}=\"#{v}\""
       end.join(", ")
-      @sample_request ||= OpenStruct.new authorization: authorization
+      mock_authorization_request(authorization)
     end
 
     def malformed_request
-      @malformed_request ||= OpenStruct.new authorization: %{Token token=}
+      mock_authorization_request(%{Token token=})
+    end
+
+    def sample_request_without_token_key(token)
+      mock_authorization_request(%{Token #{token}})
+    end
+
+    def mock_authorization_request(authorization)
+      OpenStruct.new(authorization: authorization)
     end
 
     def encode_credentials(token, options = {})

actionpack/test/controller/test_case_test.rb
@@ -713,7 +713,7 @@ XML
 
   def test_fixture_file_upload_should_be_able_access_to_tempfile
     file = fixture_file_upload(FILES_DIR + "/mona_lisa.jpg", "image/jpg")
-    assert_equal true, file.respond_to?(:tempfile) 
+    assert file.respond_to?(:tempfile), "expected tempfile should respond on fixture file object, got nothing"
   end
 
   def test_fixture_file_upload

activerecord/test/cases/callbacks_test.rb
@@ -443,7 +443,8 @@ class CallbacksTest < ActiveRecord::TestCase
     david = ImmutableDeveloper.find(1)
     assert david.valid?
     assert !david.save
-    assert_raise(ActiveRecord::RecordNotSaved) { david.save! }
+    exc = assert_raise(ActiveRecord::RecordNotSaved) { david.save! }
+    assert_equal exc.record, david
 
     david = ImmutableDeveloper.find(1)
     david.salary = 10_000_000
@@ -477,7 +478,8 @@ class CallbacksTest < ActiveRecord::TestCase
   def test_before_destroy_returning_false
     david = ImmutableDeveloper.find(1)
     assert !david.destroy
-    assert_raise(ActiveRecord::RecordNotDestroyed) { david.destroy! }
+    exc = assert_raise(ActiveRecord::RecordNotDestroyed) { david.destroy! }
+    assert_equal exc.record, david
     assert_not_nil ImmutableDeveloper.find_by_id(1)
 
     someone = CallbackCancellationDeveloper.find(1)